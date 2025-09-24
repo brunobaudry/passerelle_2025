77681602e2e8c627fae5299e384e3772bf5a824a
Bruno Baudry <bruno.baudry@bfh.ch>
2025-09-24 10:05:17 +0200

    update

--- ascii_art/code.js
+++ ascii_art/code.js
@@ -3,6 +3,8 @@ window.onload = () => {
   // Variables
   let rows = 0;
   let cols = 0;
+  const primary = "&nbsp;"; // Initial char.
+
   const calculateGridSize = () => {
       // Create a test SPAN.
     const testSpan = document.createElement('span');
@@ -18,6 +20,22 @@ window.onload = () => {
     cols = Math.floor(window.innerWidth / cellWidth) + 4;
     rows = Math.floor(window.innerHeight / cellHeight) + 1;
   };
+  const buildBrid = ()=>{
+    // Build the grid.
+    for (let i = 0; i < rows; i++) {
+      for (let j = 0; j < cols; j++) {
+        const span = document.createElement('span');
+        span.id = `id${i}-${j}`; // name from x,y.
+        span.innerHTML = primary; // fill it in with the initial character.
+        span.addEventListener('mouseover', ballChanger);
+        // Add the cell.
+        document.body.appendChild(span);
+      }
+      // new line.
+      document.body.appendChild(document.createElement('br'));
+    }
+  }
   calculateGridSize();
+  buildBrid();
   console.log(cols, rows);
 }
